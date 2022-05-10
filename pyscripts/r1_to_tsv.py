3f05d9f2829665e10c08a2403e2f5b14d26e0b07
tzlilyair <68994030+tzlilyair@users.noreply.github.com>
2022-05-10 15:30:03 +0300

    Update r1_to_tsv.py

--- pyscripts/r1_to_tsv.py
+++ pyscripts/r1_to_tsv.py
@@ -1,16 +1,15 @@
+# This script extract the barcode's sequence from the fastq files. 
+# The code takes row number three out of the four rows (in the fastq file) and saves it in a new txt file that conclude list of the barcodes. 
+
 import gzip
 import sys
 import os
 
-# os.popen('mkdir -p '+sys.argv[2])
-# os.makedirs(sys.argv[2], exist_ok=True)
 with open(sys.argv[1], 'rt') as fastq:
     with open(sys.argv[2],'w') as out:
         lines=[]
         for line in fastq:
             lines.append(line)
-            # Append lines until there are 4 (corresponding to 1 read)
-            # output just the first-two column-wise
             if len(lines)==4:
                 out.write(lines[0][1:].split(" ")[0]+"\t"+lines[1][:16].strip()+'\n')
                 lines=[]
